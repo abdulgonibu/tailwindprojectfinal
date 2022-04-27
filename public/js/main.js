566af6075fb7a9c98b44d1b876f74cf4ac8a34f8
niraapod <niraapod@gmail.com>
2022-04-28 02:24:40 +0600

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,64 +1,75 @@
-$(document).ready(function(){
-    $('#popular-course, #new-course').slick({
-            dots: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            responsive: [
-              {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  infinite: true,
-                  dots: true
-                }
-              },
-              {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2
-                }
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
-              }
-              // You can unslick at a given breakpoint now by adding:
-              // settings: "unslick"
-              // instead of a settings object
-            ]
-          });
+$(document).ready(function () {
+  $("#popular-course, #new-course").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 
-     /*********************************************
+  /*********************************************
   *  Navbar Responsive
   /*********************************************/
   $(".mobile-navbar").removeClass("hidden");
   $(".mobile-navbar").hide();
 
-  $(".navbar-toggle").on("click", function() {
+  $(".navbar-toggle").on("click", function () {
     $(".mobile-navbar").toggle("slide");
     $("#toggle-icon").toggleClass("fa-times");
   });
 
-  $(window).click(function() {
+  $(window).click(function () {
     $(".mobile-navbar").hide("slide");
     $("#toggle-icon").addClass("fa-bars-staggered");
     $("#toggle-icon").removeClass("fa-times");
   });
 
-  $(".navbar-toggle").on("click", function(event) {
+  $(".navbar-toggle").on("click", function (event) {
     event.stopPropagation();
   });
 
-  $(".mobile-navbar").on("click", function(event) {
+  $(".mobile-navbar").on("click", function (event) {
     event.stopPropagation();
   });
 
+ /*********************************************
+  *  Scroll to top
+  /*********************************************/
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll-to-top").addClass("fixed");
+      $("#scroll-to-top").removeClass("hidden");
+    } else {
+      $("#scroll-to-top").removeClass("fixed");
+      $("#scroll-to-top").addClass("hidden");
+    }
+  });
 });
- 
